435a5dd559e5972040897d348a3714fefa87c79e
Jscole95 <jscole95@gmail.com>
2017-07-11 18:43:06 -0700

    Complete

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// your code here!
+$('form').submit(function(event){
+	event.preventDefault();
+	var newarr = [];
+	var wc = 0, uwc = 0, same = 0, x = 0;
+
+	$("#user-text").val().split(" ").forEach(function(val){
+		newarr.forEach(function(val2){
+			if (val == val2)
+				same = 1;
+		})
+		if (same == 0){
+			wc++;
+			uwc++;
+			newarr.push(val);
+		}
+		else{
+			same = 0;
+			wc++;
+		}
+		x += val.length;
+	})
+	var awl = x/wc;
+
+	$("dd").empty();
+	$("dl.hidden").removeClass("hidden");
+	$("dd:nth-of-type(1)").append(wc);
+	$("dd:nth-of-type(2)").append(uwc);
+	$("dd:nth-of-type(3)").append(awl);
+})
